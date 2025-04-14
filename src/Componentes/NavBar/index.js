eea340804e11f815b330a12be155304675f67916
Pedro Navarro <phvnavarro@gmail.com>
2025-04-14 20:27:01 -0300

    First Component

---
+++ src/Componentes/NavBar/index.js
@@ -0,0 +1,18 @@
+import "./index.css";
+
+function NavBar(children,className) {
+
+    const navItems = []
+
+    return (
+        <header className="Nav-Bar">
+            <div className={className}></div>
+            <div className={className}></div>
+        </header>
+
+
+
+    )
+}
+
+export default NavBar;
